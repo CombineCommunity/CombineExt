3bc18caf9e3473d0a36a8507ac5f5fc29427dac9
Dmitry Kuznetsov <dsk1306@icloud.com>
2020-01-27 22:08:45 +0300

    Set platforms to iOS 13

--- Package.swift
+++ Package.swift
@@ -4,6 +4,7 @@ import PackageDescription
 
 let package = Package(
     name: "CombineExt",
+    platforms: [.iOS(.v13)],
     products: [.library(name: "CombineExt", targets: ["CombineExt"])],
     dependencies: [],
     targets: [
