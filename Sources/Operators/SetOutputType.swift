14339d6500fa1154d10c1d9a5aae75fca142b2b5
Jasdev Singh <jasdev@jasdev.me>
2020-04-04 18:12:45 -0400

    Update Sources/Operators/SetOutputType.swift
    Co-Authored-By: Shai Mishali <freak4pc@gmail.com>

--- Sources/Operators/SetOutputType.swift
+++ Sources/Operators/SetOutputType.swift
@@ -11,7 +11,7 @@ import Combine
 public extension Publisher where Output == Never {
     /// An output analog to [Publisher.setFailureType(to:)](https://developer.apple.com/documentation/combine/publisher/3204753-setfailuretype) for when `Output == Never`. This is especially helpful when chained after [.ignoreOutput()](https://developer.apple.com/documentation/combine/publisher/3204714-ignoreoutput) operator calls.
     ///
-    /// - Parameter outputType: The new output type for downstream.
+    /// - parameter outputType: The new output type for downstream.
     ///
     /// - Returns: A publisher with a `NewOutput` output type.
     func setOutputType<NewOutput>(to outputType: NewOutput.Type) -> Publishers.Map<Self, NewOutput> {
