52fd51a9319d23aa4fb2e11352447bbba1e93d60
Jasdev Singh <jasdev@jasdev.me>
2020-04-10 23:06:39 -0400

    `Set.store(_:)` `AnyCancellable`-constrained storage overloads.
    Figured it’d be helpful to have an anlog to the [`DisposeBag.insert(_:)` variants](https://github.com/ReactiveX/RxSwift/blob/002d325b0bdee94e7882e1114af5ff4fe1e96afa/RxSwift/Disposables/DisposeBag.swift#L100-L113).

---
+++ Sources/Utilities/Set+AnyCancellable.swift
@@ -0,0 +1,74 @@
+//
+//  Set+AnyCancellable.swift
+//  CombineExt
+//
+//  Created by Jasdev Singh on 10/04/2020.
+//  Copyright © 2020 Combine Community. All rights reserved.
+//
+
+import Combine
+
+public extension Set where Element == AnyCancellable {
+    /// Convenience storage method on `Set` for a veridic number of `AnyCancellable`s.
+    ///
+    /// `Set.store(_:)` can save repeated [AnyCancellable.store(in:)](https://developer.apple.com/documentation/combine/anycancellable/3333294-store) calls, e.g.
+    ///
+    /// ```
+    /// firstPublisher
+    ///     .sink( /* … */ )
+    ///     .store(in: &subscriptions)
+    ///
+    /// secondPublisher
+    ///     .sink( /* … */ )
+    ///         .store(in: &subscriptions)
+    ///
+    /// thirdPublisher
+    ///     .sink( /* … */ )
+    ///     .store(in: &subscriptions)
+    /// ```
+    ///
+    /// can be rewritten as
+    ///
+    /// ```
+    /// subscriptions.store(
+    ///     firstPublisher
+    ///         .sink( /* … */ ),
+    ///     secondPublisher
+    ///         .sink( /* … */ ),
+    ///     thirdPublisher
+    ///         .sink( /* … */ )
+    /// )
+    /// ```
+    ///
+    /// - parameter cancellables: The cancellables to store in the `Set`.
+    mutating func store(_ cancellables: AnyCancellable...) {
+        store(cancellables)
+    }
+
+    /// Convenience storage method on `Set` for a `Collection` of `AnyCancellable`s.
+    ///
+    /// `Set.store(_:)` can help in situations where you want to store batches of cancellables in one go, e.g.
+    ///
+    /// ```
+    /// let firstBatchOfCancellables = […]
+    ///
+    /// /* … */
+    ///
+    /// let secondBatchOfCancellables = […]
+    ///
+    /// /* … */
+    ///
+    /// let thirdBatchOfCancellables = […]
+    ///
+    /// subscriptions.store(
+    ///     firstBatchOfCancellables +
+    ///     secondBatchOfCancellables +
+    ///     thirdBatchOfCancellables
+    /// )
+    /// ```
+    ///
+    /// - parameter cancellables: The cancellables to store in the `Set`.
+    mutating func store<Collection: Swift.Collection>(_ cancellables: Collection) where Collection.Element == AnyCancellable {
+        cancellables.forEach { insert($0) }
+    }
+}
---
+++ Tests/Set+AnyCancellableTests.swift
@@ -0,0 +1,52 @@
+//
+//  Set+AnyCancellableTests.swift
+//  CombineExtPackageTests
+//
+//  Created by Jasdev Singh on 4/10/20.
+//
+
+import Combine
+import CombineExt
+import XCTest
+
+final class SetAnyCancellableTests: XCTestCase {
+    func testStorageCollectionVariant() {
+        var subscriptions = Set<AnyCancellable>()
+
+        subscriptions.store(
+            (1...3).map {
+                Just($0)
+                    .sink(receiveValue: { _ in })
+            }
+        )
+
+        XCTAssertEqual(subscriptions.count, 3)
+
+        subscriptions.removeAll()
+
+        XCTAssertTrue(subscriptions.isEmpty)
+
+        subscriptions.store([])
+
+        XCTAssertTrue(subscriptions.isEmpty)
+    }
+
+    func testStorageVariadicVariant() {
+        var subscriptions = Set<AnyCancellable>()
+
+        subscriptions.store(
+            Just(1)
+                .sink(receiveValue: { _ in }),
+            Just(2)
+                .sink(receiveValue: { _ in }),
+            Just(3)
+                .sink(receiveValue: { _ in })
+        )
+
+        XCTAssertEqual(subscriptions.count, 3)
+
+        subscriptions.removeAll()
+
+        XCTAssertTrue(subscriptions.isEmpty)
+    }
+}
