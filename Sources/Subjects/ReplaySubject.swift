6e65abadfb91ea284ccdb7d9180f5660550b86f8
Jasdev Singh <jasdevs@gmail.com>
2020-04-19 16:40:02 -0400

    Adds ReplaySubject and Publisher.share(replay:) (#23)

---
+++ Sources/Subjects/ReplaySubject.swift
@@ -0,0 +1,128 @@
+//
+//  ReplaySubject.swift
+//  CombineExt
+//
+//  Created by Jasdev Singh on 13/04/2020.
+//  Copyright © 2020 Combine Community. All rights reserved.
+//
+
+import Combine
+
+/// A `ReplaySubject` is a subject that can buffer one or more values. It stores value events, up to its `bufferSize` in a
+/// first-in-first-out manner and then replays it to
+/// future subscribers and also forwards completion events.
+///
+/// The implementation borrows heavily from [Entwine’s](https://github.com/tcldr/Entwine/blob/b839c9fcc7466878d6a823677ce608da998b95b9/Sources/Entwine/Operators/ReplaySubject.swift).
+public final class ReplaySubject<Output, Failure: Error>: Subject {
+    public typealias Output = Output
+    public typealias Failure = Failure
+
+    private let bufferSize: Int
+    private var buffer = [Output]()
+
+    // Keeping track of all live subscriptions, so `send` events can be forwarded to them.
+    private var subscriptions = [Subscription<AnySubscriber<Output, Failure>>]()
+
+    // We also track subscriber identifiers, to more quickly bottom-out double subscribes instead of having to do a
+    // linear pass over `subscriptions`.
+    private var subscriberIdentifiers = Set<CombineIdentifier>()
+
+    private var completion: Subscribers.Completion<Failure>?
+    private var isActive: Bool { completion == nil }
+
+    /// Create a `ReplaySubject`, buffering up to `bufferSize` values and replaying them to new subscribers
+    /// - Parameter bufferSize: The maximum number of value events to buffer and replay to all future subscribers.
+    public init(bufferSize: Int) {
+        self.bufferSize = bufferSize
+    }
+
+    public func send(_ value: Output) {
+        guard isActive else { return }
+
+        buffer.append(value)
+
+        if buffer.count > bufferSize {
+            buffer.removeFirst()
+        }
+
+        subscriptions.forEach { $0.forwardValueToBuffer(value) }
+    }
+
+    public func send(completion: Subscribers.Completion<Failure>) {
+        guard isActive else { return }
+
+        self.completion = completion
+
+        subscriptions.forEach { $0.forwardCompletionToBuffer(completion) }
+    }
+
+    public func send(subscription: Combine.Subscription) {
+        subscription.request(.unlimited)
+    }
+
+    public func receive<Subscriber: Combine.Subscriber>(subscriber: Subscriber) where Failure == Subscriber.Failure, Output == Subscriber.Input {
+        let subscriberIdentifier = subscriber.combineIdentifier
+
+        guard !subscriberIdentifiers.contains(subscriberIdentifier) else {
+            subscriber.receive(subscription: Subscriptions.empty)
+            return
+        }
+
+        let subscription = Subscription(downstream: AnySubscriber(subscriber)) { [weak self] in
+            guard let self = self,
+                  let subscriptionIndex = self.subscriptions
+                                              .firstIndex(where: { $0.innerSubscriberIdentifier == subscriberIdentifier }) else { return }
+
+            self.subscriberIdentifiers.remove(subscriberIdentifier)
+            self.subscriptions.remove(at: subscriptionIndex)
+        }
+
+        subscriberIdentifiers.insert(subscriberIdentifier)
+        subscriptions.append(subscription)
+
+        subscriber.receive(subscription: subscription)
+        subscription.replay(buffer, completion: completion)
+    }
+}
+
+extension ReplaySubject {
+    final class Subscription<Downstream: Subscriber>: Combine.Subscription where Output == Downstream.Input, Failure == Downstream.Failure {
+        private var demandBuffer: DemandBuffer<Downstream>?
+        private var cancellationHandler: (() -> Void)?
+
+        fileprivate let innerSubscriberIdentifier: CombineIdentifier
+
+        init(downstream: Downstream, cancellationHandler: (() -> Void)?) {
+            self.demandBuffer = DemandBuffer(subscriber: downstream)
+            self.innerSubscriberIdentifier = downstream.combineIdentifier
+            self.cancellationHandler = cancellationHandler
+        }
+
+        func replay(_ buffer: [Output], completion: Subscribers.Completion<Failure>?) {
+            buffer.forEach(forwardValueToBuffer)
+
+            if let completion = completion {
+                forwardCompletionToBuffer(completion)
+            }
+        }
+
+        func forwardValueToBuffer(_ value: Output) {
+            _ = demandBuffer?.buffer(value: value)
+        }
+
+        func forwardCompletionToBuffer(_ completion: Subscribers.Completion<Failure>) {
+            demandBuffer?.complete(completion: completion)
+        }
+
+        func request(_ demand: Subscribers.Demand) {
+            _ = demandBuffer?.demand(demand)
+        }
+
+        func cancel() {
+            cancellationHandler?()
+            cancellationHandler = nil
+
+            demandBuffer = nil
+        }
+    }
+}
---
+++ Tests/ShareReplayTests.swift
@@ -0,0 +1,214 @@
+//
+//  ShareReplayTests.swift
+//  CombineExtTests
+//
+//  Created by Jasdev Singh on 4/13/20.
+//
+
+import Combine
+import CombineExt
+import XCTest
+
+final class ShareReplayTests: XCTestCase {
+    private var subscriptions = Set<AnyCancellable>()
+
+    private enum AnError: Error {
+        case someError
+    }
+
+    func testSharingNoReplay() {
+        var subscribeCount = 0
+
+        let publisher = Publishers.Create<Int, Never> { subscriber in
+            subscribeCount += 1
+            subscriber.send(1)
+            subscriber.send(2)
+            subscriber.send(3)
+            subscriber.send(completion: .finished)
+
+            return AnyCancellable { }
+        }
+        .share(replay: 0)
+
+        publisher
+            .sink(receiveValue: { _ in })
+            .store(in: &subscriptions)
+
+        publisher
+            .sink(receiveValue: { _ in })
+            .store(in: &subscriptions)
+
+        publisher
+            .sink(receiveValue: { _ in })
+            .store(in: &subscriptions)
+
+        XCTAssertEqual(subscribeCount, 1)
+    }
+
+    func testSharingSingleReplay() {
+        let subject = CurrentValueSubject<Int, Never>(1)
+
+        let publisher = subject
+            .share(replay: 1)
+
+        var results = [Int]()
+
+        publisher
+            .sink(receiveValue: { results.append($0) })
+            .store(in: &subscriptions)
+
+        subject.send(2)
+
+        XCTAssertEqual(results, [1, 2])
+    }
+
+    func testSharingManyReplay() {
+        let subject = PassthroughSubject<Int, Never>()
+
+        var results1 = [Int]()
+        var results2 = [Int]()
+
+        let publisher = subject
+            .share(replay: 3)
+
+        publisher
+            .sink(receiveValue: { results1.append($0) })
+            .store(in: &subscriptions)
+
+        subject.send(1)
+        subject.send(2)
+        subject.send(3)
+        subject.send(4)
+
+        publisher
+            .sink(receiveValue: { results2.append($0) })
+            .store(in: &subscriptions)
+
+        XCTAssertEqual(results1, [1, 2, 3, 4])
+        XCTAssertEqual(results2, [2, 3, 4])
+    }
+
+    func testSharingWithFinishedEvent() {
+        let subject = PassthroughSubject<Int, Never>()
+
+        var results1 = [Int]()
+        var completions1 = [Subscribers.Completion<Never>]()
+
+        var results2 = [Int]()
+        var completions2 = [Subscribers.Completion<Never>]()
+
+        let publisher = subject
+            .share(replay: 3)
+
+        publisher
+            .sink(
+                receiveCompletion: { completions1.append($0) },
+                receiveValue: { results1.append($0) }
+            )
+            .store(in: &subscriptions)
+
+        subject.send(1)
+        subject.send(2)
+        subject.send(3)
+        subject.send(4)
+        subject.send(completion: .finished)
+
+        publisher
+            .sink(
+                receiveCompletion: { completions2.append($0) },
+                receiveValue: { results2.append($0) }
+            )
+            .store(in: &subscriptions)
+
+        XCTAssertEqual(results1, [1, 2, 3, 4])
+        XCTAssertEqual(completions1, [.finished])
+
+        XCTAssertEqual(results2, [2, 3, 4])
+        XCTAssertEqual(completions2, [.finished])
+    }
+
+    func testSharingWithErrorEvent() {
+        let subject = PassthroughSubject<Int, AnError>()
+
+        var results1 = [Int]()
+        var completions1 = [Subscribers.Completion<AnError>]()
+
+        var results2 = [Int]()
+        var completions2 = [Subscribers.Completion<AnError>]()
+
+        let publisher = subject
+            .share(replay: 3)
+
+        publisher
+            .sink(
+                receiveCompletion: { completions1.append($0) },
+                receiveValue: { results1.append($0) }
+            )
+            .store(in: &subscriptions)
+
+        subject.send(1)
+        subject.send(2)
+        subject.send(3)
+        subject.send(4)
+        subject.send(completion: .failure(.someError))
+
+        publisher
+            .sink(
+                receiveCompletion: { completions2.append($0) },
+                receiveValue: { results2.append($0) }
+            )
+            .store(in: &subscriptions)
+
+        XCTAssertEqual(results1, [1, 2, 3, 4])
+        XCTAssertEqual(completions1, [.failure(.someError)])
+
+        XCTAssertEqual(results2, [2, 3, 4])
+        XCTAssertEqual(completions2, [.failure(.someError)])
+    }
+
+    func testFinishWithNoReplay() {
+        let subject = PassthroughSubject<Int, Never>()
+
+        var results = [Int]()
+        var completions = [Subscribers.Completion<Never>]()
+
+        let publisher = subject
+            .share(replay: 1)
+
+        publisher
+            .sink(
+                receiveCompletion: { completions.append($0) },
+                receiveValue: { results.append($0) }
+            )
+            .store(in: &subscriptions)
+
+        subject.send(completion: .finished)
+        subject.send(1)
+
+        XCTAssertTrue(results.isEmpty)
+        XCTAssertEqual(completions, [.finished])
+    }
+
+    func testErrorWithNoReplay() {
+        let subject = PassthroughSubject<Int, AnError>()
+
+        var results = [Int]()
+        var completions = [Subscribers.Completion<AnError>]()
+
+        let publisher = subject
+            .share(replay: 1)
+
+        publisher
+            .sink(
+                receiveCompletion: { completions.append($0) },
+                receiveValue: { results.append($0) }
+            )
+            .store(in: &subscriptions)
+
+        subject.send(completion: .failure(.someError))
+        subject.send(1)
+
+        XCTAssertTrue(results.isEmpty)
+        XCTAssertEqual(completions, [.failure(.someError)])
+    }
+}
