3d4ad5a945e3211df11db1c3da25377120bae9a7
Dmitry Kuznetsov <dsk1306@icloud.com>
2020-09-10 14:39:29 +0300

    Updated PassthroughRelayTests

--- Tests/PassthroughRelayTests.swift
+++ Tests/PassthroughRelayTests.swift
@@ -92,7 +92,7 @@ class PassthroughRelayTests: XCTestCase {
         XCTAssertEqual(values, ["1", "2", "3"])
     }
 
-    func testSubscribePublisher2() {
+    func testSubscribeRelay_Passthroughs() {
         var completed = false
 
         let input = PassthroughRelay<String>()
@@ -112,6 +112,50 @@ class PassthroughRelayTests: XCTestCase {
 
         XCTAssertFalse(completed)
         XCTAssertEqual(values, ["1", "2", "3"])
+    }
+
+    func testSubscribeRelay_PassthroughToCurrentValue() {
+        var completed = false
+
+        let input = PassthroughRelay<String>()
+        let output = CurrentValueRelay<String>("initial")
+
+        input
+            .subscribe(output)
+            .store(in: &subscriptions)
+        output
+            .sink(receiveCompletion: { _ in completed = true },
+                  receiveValue: { self.values.append($0) })
+            .store(in: &subscriptions)
+
+        input.accept("1")
+        input.accept("2")
+        input.accept("3")
+
+        XCTAssertFalse(completed)
+        XCTAssertEqual(values, ["initial", "1", "2", "3"])
+    }
+
+    func testSubscribeRelay_CurrentValueToPassthrough() {
+        var completed = false
+
+        let input = CurrentValueRelay<String>("initial")
+        let output = PassthroughRelay<String>()
+
+        input
+            .subscribe(output)
+            .store(in: &subscriptions)
+        output
+            .sink(receiveCompletion: { _ in completed = true },
+                  receiveValue: { self.values.append($0) })
+            .store(in: &subscriptions)
+
+        input.accept("1")
+        input.accept("2")
+        input.accept("3")
+
+        XCTAssertFalse(completed)
+        XCTAssertEqual(values, ["initial", "1", "2", "3"])
     }
 }
 #endif
