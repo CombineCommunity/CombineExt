0ee50fa77ee70994661d3002ce593db627816c50
Dmitry Kuznetsov <dsk1306@icloud.com>
2020-09-12 18:44:57 +0300

    Added more unit tests (#57)

--- Tests/CurrentValueRelayTests.swift
+++ Tests/CurrentValueRelayTests.swift
@@ -77,10 +77,32 @@ class CurrentValueRelayTests: XCTestCase {
         XCTAssertEqual(values, ["initial", "1", "2", "3"])
     }
 
-    func testSubscribePublisher2() {
+    func testSubscribeRelay_CurrentValues() {
         var completed = false
 
         let input = CurrentValueRelay<String>("initial")
+        let output = CurrentValueRelay<String>("initial")
+
+        input
+            .subscribe(output)
+            .store(in: &subscriptions)
+        output
+            .sink(receiveCompletion: { _ in completed = true },
+                  receiveValue: { self.values.append($0) })
+            .store(in: &subscriptions)
+
+        input.accept("1")
+        input.accept("2")
+        input.accept("3")
+
+        XCTAssertFalse(completed)
+        XCTAssertEqual(values, ["initial", "1", "2", "3"])
+    }
+
+    func testSubscribeRelay_PassthroughToCurrentValue() {
+        var completed = false
+
+        let input = PassthroughRelay<String>()
         let output = CurrentValueRelay<String>("initial")
 
         input
