dea024541415fe3505b2d0afa0080041e1e5026c
Joe Walsh <joewalsh@users.noreply.github.com>
2020-11-23 08:47:01 -0500

    Add tests for tryScanInto and tryReduceInto

--- Tests/ReduceIntoTests.swift
+++ Tests/ReduceIntoTests.swift
@@ -13,15 +13,48 @@ import Combine
 class ReduceIntoTests: XCTestCase {
     var subscription: Cancellable?
     
+    enum ReduceIntoTestError: Error {
+        case generic
+    }
+    
     func testReduceInto() {
         let subject = PassthroughSubject<Int, Never>()
-
+        
         subscription = subject
-          .reduce(into: 0) { $0 += $1 }
-          .sink(receiveValue: {
-            XCTAssertEqual(15, $0, "Reduce into should only emit the resulting value once")
-          })
-
+            .reduce(into: 0) { $0 += $1 }
+            .sink(receiveValue: {
+                XCTAssertEqual(15, $0, "Reduce into should only emit the resulting value once")
+            })
+        
+        subject.send(1)
+        subject.send(2)
+        subject.send(3)
+        subject.send(4)
+        subject.send(5)
+        subject.send(completion: .finished)
+    }
+    
+    func testTryReduceInto() {
+        let subject = PassthroughSubject<Int, ReduceIntoTestError>()
+        
+        subscription = subject
+            .tryReduce(into: 0, { (sum, val) in
+                guard val < 3 else {
+                    throw ReduceIntoTestError.generic
+                }
+                sum += val
+            })
+            .sink(receiveCompletion: { completion in
+                switch completion {
+                case .failure(let error):
+                    XCTAssertEqual(error as? ReduceIntoTestError, .generic, "Reduce into error should match the thrown error")
+                case .finished:
+                    XCTFail("Reduce into shouldn't finish if an error is thrown")
+                }
+            }, receiveValue: { _ in
+                XCTFail("Reduce into shouldn't receive value if subject is never completed")
+            })
+        
         subject.send(1)
         subject.send(2)
         subject.send(3)
--- Tests/ScanIntoTests.swift
+++ Tests/ScanIntoTests.swift
@@ -13,22 +13,55 @@ import Combine
 class ScanIntoTests: XCTestCase {
     var subscription: Cancellable?
     
+    enum ScanIntoTestError: Error {
+        case generic
+    }
+    
     func testScanInto() {
         let subject = PassthroughSubject<Int, Never>()
         let values = Array(1...5)
         var index = 0
         var sum = 0
         subscription = subject
-          .scan(into: 0) { $0 += $1 }
-          .sink(receiveValue: {
-            guard index < values.endIndex else {
-                return
-            }
-            sum += values[index]
-            XCTAssertEqual(sum, $0, "Scan into should emit the correct value")
-            index += 1
-          })
+            .scan(into: 0) { $0 += $1 }
+            .sink(receiveValue: {
+                guard index < values.endIndex else {
+                    return
+                }
+                sum += values[index]
+                XCTAssertEqual(sum, $0, "Scan into should emit the correct value")
+                index += 1
+            })
         values.forEach { subject.send($0) }
         XCTAssertEqual(index, values.endIndex, "Scan into should emit a value for every upstream value")
+    }
+    
+    func testTryScanInto() {
+        let subject = PassthroughSubject<Int, Error>()
+        let values = Array(1...5)
+        var index = 0
+        var sum = 0
+        subscription = subject
+            .tryScan(into: 0, { (sum, val) in
+                guard val < 3 else {
+                    throw ScanIntoTestError.generic
+                }
+                sum += val
+            })
+            .sink(receiveCompletion: { completion in
+                switch completion {
+                case .failure(let error):
+                    XCTAssertEqual(error as? ScanIntoTestError, .generic, "Scan into error should match the thrown error")
+                case .finished:
+                    XCTFail("Scan into shouldn't finish if an error is thrown")
+                }
+            }, receiveValue: {
+                sum += values[index]
+                XCTAssertLessThan($0, 4, "Scan into shouldn't emit values after the error")
+                XCTAssertEqual(sum, $0, "Scan into should emit the correct value")
+                index += 1
+            })
+        
+        values.forEach { subject.send($0) }
     }
 }
