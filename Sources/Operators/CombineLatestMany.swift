cd120ce9a4d4d0ba8b73906b639d98a5d933ab94
fischman-bcny <ami-github-emails-aaaaf75vldllkcgdrw2p2ypzqe@thebrowsercompany.slack.com>
2022-06-09 10:43:19 -0700

    combineLatest & zip: make scalable using divide-and-conquer. (#130)
    * combineLatest: make scalable using divide-and-conquer.
    
    Previously the implementation would trigger a stack overflow on 15000
    publishers because the implementation combined its inputs linearly.
    This commit replaces that with repeated (hierarchical) 4-way merges,
    which lets the same scaled test scenario go to 1e7 publishers with no
    issues (other than taking 28s to run the test on my M1 Pro, hence the
    choice to commit the test with only 1e5 publishers). Test execution
    wall-time scales linearly with publisher count.
    
    Re: "4-way merge":
    - Why not use a higher value? 4 is the highest arity offered by
      Combine's Publisher.combineLatest.
    - Why not use a lower value? Using 2-way merges instead of 4-way still
      lets the 1e7 test case pass, but is ~60% slower (1e7 publishers take
      45s insted of 28s).
    
    * zip: make scalable using divide-and-conquer, too.
    
    * Restore signatures for Publisher extensions.
    
    * Minor touch-ups
    
    Minor touch-ups
    
    * drop now-redundant isEmpty checks
    
    Co-authored-by: Shai Mishali <freak4pc@gmail.com>

--- Sources/Operators/CombineLatestMany.swift
+++ Sources/Operators/CombineLatestMany.swift
@@ -21,19 +21,12 @@ public extension Publisher {
     func combineLatest<Others: Collection>(with others: Others)
         -> AnyPublisher<[Output], Failure>
         where Others.Element: Publisher, Others.Element.Output == Output, Others.Element.Failure == Failure {
-        let seed = map { [$0] }.eraseToAnyPublisher()
-
-        return others.reduce(seed) { combined, next in
-            combined
-                .combineLatest(next)
-                .map { $0 + [$1] }
-                .eraseToAnyPublisher()
-        }
+        ([self.eraseToAnyPublisher()] + others.map { $0.eraseToAnyPublisher() }).combineLatest()
     }
 
     /// Projects `self` and a `Collection` of `Publisher`s onto a type-erased publisher that chains `combineLatest` calls on
     /// the inner publishers. This is a variadic overload on Combine’s variants that top out at arity three.
-	///
+    ///
     /// - parameter others: A `Collection`-worth of other publishers with matching output and failure types to combine with.
     ///
     /// - returns: A type-erased publisher with value events from `self` and each of the inner publishers `combineLatest`’d
@@ -53,19 +46,52 @@ public extension Collection where Element: Publisher {
     /// - returns: A type-erased publisher with value events from each of the inner publishers `combineLatest`’d
     /// together in an array.
     func combineLatest() -> AnyPublisher<[Element.Output], Element.Failure> {
-        switch count {
-        case 0:
-            return Empty().eraseToAnyPublisher()
-        case 1:
-            return self[startIndex]
-                .combineLatest(with: [Element]())
-        default:
-            let first = self[startIndex]
-            let others = self[index(after: startIndex)...]
-
-            return first
-                .combineLatest(with: others)
+        var wrapped = map { $0.map { [$0] }.eraseToAnyPublisher() }
+        while wrapped.count > 1 {
+            wrapped = makeCombinedQuads(input: wrapped)
         }
+        return wrapped.first?.eraseToAnyPublisher() ?? Empty().eraseToAnyPublisher()
+    }
+}
+
+// MARK: - Private helpers
+@available(OSX 10.15, iOS 13.0, tvOS 13.0, watchOS 6.0, *)
+/// CombineLatest an array of input publishers in four-somes.
+///
+/// - parameter input: An array of publishers
+private func makeCombinedQuads<Output, Failure: Swift.Error>(
+    input: [AnyPublisher<[Output], Failure>]
+) -> [AnyPublisher<[Output], Failure>] {
+    // Iterate over the array of input publishers in steps of four
+    sequence(
+        state: input.makeIterator(),
+        next: { it in it.next().map { ($0, it.next(), it.next(), it.next()) } }
+    )
+    .map { quad in
+        // Only one publisher
+        guard let second = quad.1 else { return quad.0 }
+
+        // Two publishers
+        guard let third = quad.2 else {
+            return quad.0
+                .combineLatest(second)
+                .map { $0.0 + $0.1 }
+                .eraseToAnyPublisher()
+        }
+
+        // Three publishers
+        guard let fourth = quad.3 else {
+            return quad.0
+                .combineLatest(second, third)
+                .map { $0.0 + $0.1 + $0.2 }
+                .eraseToAnyPublisher()
+        }
+
+        // Four publishers
+        return quad.0
+            .combineLatest(second, third, fourth)
+            .map { $0.0 + $0.1 + $0.2 + $0.3 }
+            .eraseToAnyPublisher()
     }
 }
 #endif
--- Sources/Operators/ZipMany.swift
+++ Sources/Operators/ZipMany.swift
@@ -21,16 +21,7 @@ public extension Publisher {
     func zip<Others: Collection>(with others: Others)
         -> AnyPublisher<[Output], Failure>
         where Others.Element: Publisher, Others.Element.Output == Output, Others.Element.Failure == Failure {
-        let seed = map { [$0] }.eraseToAnyPublisher()
-
-        return others
-            .reduce(seed) { zipped, next in
-                zipped
-                    .zip(next)
-                    .map { $0.0 + [$0.1] }
-                    .eraseToAnyPublisher()
-            }
-            .eraseToAnyPublisher()
+        ([self.eraseToAnyPublisher()] + others.map { $0.eraseToAnyPublisher() }).zip()
     }
 
     /// A variadic overload on `Publisher.zip(with:)`.
@@ -49,17 +40,51 @@ public extension Collection where Element: Publisher {
     ///
     /// - returns: A type-erased publisher with value events from each of the inner publishers zipped together in an array.
     func zip() -> AnyPublisher<[Element.Output], Element.Failure> {
-        switch count {
-        case 0:
-            return Empty().eraseToAnyPublisher()
-        case 1:
-            return self[startIndex].zip(with: [Element]())
-        default:
-            let first = self[startIndex]
-            let others = self[index(after: startIndex)...]
-
-            return first.zip(with: others)
+        var wrapped = map { $0.map { [$0] }.eraseToAnyPublisher() }
+        while wrapped.count > 1 {
+            wrapped = makeZippedQuads(input: wrapped)
         }
+        return wrapped.first?.eraseToAnyPublisher() ?? Empty().eraseToAnyPublisher()
+    }
+}
+
+// MARK: - Private helper
+@available(OSX 10.15, iOS 13.0, tvOS 13.0, watchOS 6.0, *)
+/// Zip an array of input publishers in four-somes.
+///
+/// - parameter input: An array of publishers
+private func makeZippedQuads<Output, Failure: Swift.Error>(
+    input: [AnyPublisher<[Output], Failure>]
+) -> [AnyPublisher<[Output], Failure>] {
+    sequence(
+        state: input.makeIterator(),
+        next: { it in it.next().map { ($0, it.next(), it.next(), it.next()) } }
+    )
+    .map { quad in
+        // Only one publisher
+        guard let second = quad.1 else { return quad.0 }
+
+        // Two publishers
+        guard let third = quad.2 else {
+            return quad.0
+                .zip(second)
+                .map { $0.0 + $0.1 }
+                .eraseToAnyPublisher()
+        }
+
+        // Three publishers
+        guard let fourth = quad.3 else {
+            return quad.0
+                .zip(second, third)
+                .map { $0.0 + $0.1 + $0.2 }
+                .eraseToAnyPublisher()
+        }
+
+        // Four publishers
+        return quad.0
+            .zip(second, third, fourth)
+            .map { $0.0 + $0.1 + $0.2 + $0.3 }
+            .eraseToAnyPublisher()
     }
 }
 #endif
