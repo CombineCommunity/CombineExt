ec7be9e44dd0a31de3ffb3f7ab69f42da80a494b
Andrea Altea <oni.zerone@gmail.com>
2023-05-18 08:28:38 +0200

    Feat: add an extension to subscribe a KeyValuePath to a Publisher

---
+++ Sources/Convenience/KeyValueCodingAndObservingSubscribing.swift
@@ -0,0 +1,37 @@
+//
+//  KeyValueCodingAndObservingSubscribing.swift
+//  CombineExt
+//
+//  Created by Andrea Altea on 18/05/23.
+//
+
+#if canImport(Combine)
+import Foundation
+import Combine
+
+@available(OSX 10.15, iOS 13.0, tvOS 13.0, watchOS 6.0, *)
+protocol KeyValueCodingAndObservingSubscribing: AnyObject where Self : ObjectiveC.NSObject { }
+
+@available(OSX 10.15, iOS 13.0, tvOS 13.0, watchOS 6.0, *)
+extension KeyValueCodingAndObservingSubscribing {
+
+    /// A method to subscribe a KeyValuePath to a Publisher.
+    func subscribe<P: Publisher, Value>(at keyPath: ReferenceWritableKeyPath<Self, Value>, to publisher: P) -> AnyCancellable where P.Output == Value, P.Failure == Never {
+        subscribe(at: keyPath, to: publisher, on: RunLoop.main)
+    }
+
+    /// A method to subscribe a KeyValuePath to a Publisher.
+    func subscribe<P: Publisher, S: Scheduler, Value>(at keyPath: ReferenceWritableKeyPath<Self, Value>, to publisher: P, on scheduler: S) -> AnyCancellable where P.Output == Value, P.Failure == Never {
+        
+        publisher
+            .receive(on: scheduler)
+            .sink { [weak self] value in
+                self?[keyPath: keyPath] = value
+            }
+    }
+}
+
+@available(OSX 10.15, iOS 13.0, tvOS 13.0, watchOS 6.0, *)
+extension NSObject: KeyValueCodingAndObservingSubscribing { }
+
+#endif
