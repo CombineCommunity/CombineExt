674318e54be6a34b2674a4e34d898c73adb5cb68
Shai Mishali <freak4pc@gmail.com>
2026-01-20 19:25:43 +0200

    Migrate to Swift 6 and update dependencies

--- Package.swift
+++ Package.swift
@@ -1,26 +1,30 @@
-// swift-tools-version:5.3
-
+// swift-tools-version:6.0
 import PackageDescription
 
 let package = Package(
     name: "CombineExt",
     platforms: [
-        .iOS(.v13), .tvOS(.v10), .macOS(.v10_12), .watchOS(.v3)
+        .iOS(.v13),
+        .tvOS(.v13),
+        .macOS(.v10_15),
+        .watchOS(.v6),
     ],
     products: [
         .library(name: "CombineExt", targets: ["CombineExt"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/pointfreeco/combine-schedulers", from: "0.8.0"),
+        .package(url: "https://github.com/pointfreeco/combine-schedulers", from: "1.1.0"),
     ],
     targets: [
         .target(name: "CombineExt", dependencies: [], path: "Sources"),
-        .testTarget(name: "CombineExtTests",
-                    dependencies: [
-                        "CombineExt",
-                        .product(name: "CombineSchedulers", package: "combine-schedulers")
-                    ],
-                    path: "Tests"),
+        .testTarget(
+            name: "CombineExtTests",
+            dependencies: [
+                "CombineExt",
+                .product(name: "CombineSchedulers", package: "combine-schedulers"),
+            ],
+            path: "Tests"
+        ),
     ],
-    swiftLanguageVersions: [.v5]
+    swiftLanguageModes: [.v6]
 )
